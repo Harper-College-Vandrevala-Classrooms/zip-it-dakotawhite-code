34a476f0fb65676dc50643581ff0256e606f595f
dakotawhite-code <wd53169@mail.harpercollege.edu>
2024-10-13 18:03:57 -0500

    done

--- src/test/java/com/csc/ZipperTest.java
+++ src/test/java/com/csc/ZipperTest.java
@@ -6,7 +6,6 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
-
 public class ZipperTest {
   Zipper zipper;
 
