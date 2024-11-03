21000d9df71cda6e0abe27722e652e11023b461b
dakotawhite-code <wd53169@mail.harpercollege.edu>
2024-11-03 11:47:44 -0600

    portfolio 2

--- src/main/java/com/csc/Zipper.java
+++ src/main/java/com/csc/Zipper.java
@@ -1,6 +1,7 @@
 package com.csc;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
 public class Zipper {
        
@@ -17,6 +18,17 @@ public class Zipper {
             return newlist;
         }
 
+        public static <x, y> HashMap<x, y> hashmapify(List<x> keys, List<y> values) {
+            if (keys.size() != values.size()) {
+                throw new IllegalArgumentException("The size of keys and values must be the same.");
+            }
+    
+            HashMap<x, y> map = new HashMap<>();
+            for (int i = 0; i < keys.size(); i++) {
+                map.put(keys.get(i), values.get(i));
+            }
+            return map;
+        }
     public static void main(String[] args) {
         List<Integer> n1 = List.of(1, 3, 5);
         List<Integer> n2 = List.of(2, 4, 6, 8);
@@ -27,6 +39,11 @@ public class Zipper {
         List<String> secondary = List.of("Orange", "Green", "Purple");
         List<String> colors = zip(primary, secondary);
         System.out.println(colors);
-        }
-    }
-    
+
+        List<String> colorsList = List.of("White", "Black", "Brown", "Pink");
+        List<Integer> nums = List.of(1, 3, 5, 7);
+        HashMap<String, Integer> map = hashmapify(colorsList, nums);
+        System.out.println("{White=" + map.get("White") + ", Black=" + map.get("Black") +
+        ", Orange=" + map.get("Brown") + ", Pink=" + map.get("Pink") + "}");
+    }        
+}
